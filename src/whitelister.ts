40b4d9aa616721c797a8825750121aaa07269ee7
naufik <nauffik@gmail.com>
2017-12-18 23:10:22 +0700

    prepared for UUID based whitelisting.

--- src/whitelister.ts
+++ src/whitelister.ts
@@ -1,5 +1,8 @@
 import * as Path from 'path';
-import * as Filesystem from 'fs'
+import * as Http from 'http';
+import * as Filesystem from 'fs';
+import * as Locutus from 'locutus';
+import * as Crypto from 'crypto';
 import Validator from './auth/ivalidator';
 
 /** Insert your relative path to minecraft's whitelist.txt here */
@@ -40,6 +43,9 @@ export default class Whitelister {
 				return false;
 			}
 		}
+		
+		this.getUUID(name, false);
+
 		if (!this.isWhiteListed(name)) {
 			try {
 				Filesystem.appendFileSync(this.pathToFile, name + "\n");
@@ -58,4 +64,24 @@ export default class Whitelister {
 		return this.read().includes(name);
 	}
 
+	private getUUID(player: string, online: boolean): string {
+		if (online) {
+			/** set post request to Minecraft's UUID database */
+			return "";
+		} else {
+			let uuid: string;
+
+			/** Call UUID API */
+
+			/** Try to manually compute UUID, this algorithm was extracted from Java Minecraft's source code - however */
+			/** this seems to be incomplete as UUID's made this way are missing the two first characters. Will find ways to
+				calculate them because an API call is more expensive. */
+			// let md5 = Locutus.php.strings.hex2bin(Crypto.createHash('md5').update("OfflinePlayer:" + player).digest("hex"));
+			// let char6 = String.fromCharCode(md5.charCodeAt(6) & 0x0f | 0x30);
+			// let char8 = String.fromCharCode(md5.charCodeAt(8) & 0x3f | 0x80);
+			// uuid = md5.substring(1,6) + char6 + md5.charAt(7) + char8 + md5.substring(9);
+
+			return uuid;
+		}
+	}
 }
